e2380e9e230cbf48f38a454e14f3d8aa8874fc23
Elias Oehen <oee@bbtsoftware.ch>
2020-11-12 10:31:02 +0100

    Improve ArgumentLessThanGuardTests

--- src/oehen.arguard.test/ArgumentLessThanGuardTest.cs
+++ src/oehen.arguard.test/ArgumentLessThanGuardTest.cs
@@ -19,6 +19,18 @@ namespace oehen.arguard
         }
 
         [Fact]
+        public void ThrowIfIsLessThanZero_ShouldThrowArgumentOutOfRangeException_IfArgumentIsMinusTwo()
+        {
+            const int argument = -2;
+            var exception = Assert.Throws<ArgumentOutOfRangeException>(() =>
+            {
+                argument.ThrowIfIsLessThanZero(nameof(argument));
+            });
+            exception.Message.Should().Be("Argument is less than 0. (Parameter 'argument')" +
+                                          Environment.NewLine + "Actual value was -2.");
+        }
+
+        [Fact]
         public void ThrowIfIsLessOrEqualThanZero_ShouldThrowArgumentOutOfRangeException_IfArgumentIsZero()
         {
             const int argument = 0;
@@ -31,7 +43,18 @@ namespace oehen.arguard
         }
 
         [Fact]
-        public void ThrowIfIsLessOrEqualThanZero_ShouldNotThrowArgumentOutOfRangeException_IfArgumentIsOne()
+        public void ThrowIfIsLessThanZero_ShouldNotThrowArgumentOutOfRangeException_IfArgumentIsZero()
+        {
+            const int argument = 0;
+            var exception = Record.Exception(() =>
+            {
+                argument.ThrowIfIsLessThanZero(nameof(argument));
+            });
+            Assert.Null(exception);
+        }
+
+        [Fact]
+        public void ThrowIfIsLessOrEqualThanZero_ShouldNotThrowArgumentOutOfRangeException_IfArgumentIsMoreThanZero()
         {
             const int argument = 1;
             var exception = Record.Exception(() =>
@@ -41,5 +64,15 @@ namespace oehen.arguard
             Assert.Null(exception);
         }
 
+        [Fact]
+        public void ThrowIfIsLessThanZero_ShouldNotThrowArgumentOutOfRangeException_IfArgumentIsMoreThanZero()
+        {
+            const int argument = 1;
+            var exception = Record.Exception(() =>
+            {
+                argument.ThrowIfIsLessThanZero(nameof(argument));
+            });
+            Assert.Null(exception);
+        }
     }
 }
