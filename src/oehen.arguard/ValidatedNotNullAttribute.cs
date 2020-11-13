af0b7c0686dcd9227fe4cf108b0b3c8422e17593
eoehen <49316770+eoehen@users.noreply.github.com>
2020-11-13 19:54:48 +0100

    Improve source code documentation (#53)

--- src/oehen.arguard/ValidatedNotNullAttribute.cs
+++ src/oehen.arguard/ValidatedNotNullAttribute.cs
@@ -3,6 +3,9 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace oehen.arguard
 {
+    /// <summary>
+    /// Validated not null attribute.
+    /// </summary>
     [ExcludeFromCodeCoverage]
     [AttributeUsage(AttributeTargets.Parameter)]
     public sealed class ValidatedNotNullAttribute : Attribute
