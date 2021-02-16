5752e64d8642ff01062950b2d3f2055ef347ce13
Elias Oehen <oee@bbtsoftware.ch>
2021-02-16 13:59:43 +0100

    Add example documentation

--- src/oehen.arguard/ArgumentEnumerableGuard.cs
+++ src/oehen.arguard/ArgumentEnumerableGuard.cs
@@ -15,6 +15,14 @@ namespace oehen.arguard
         /// </summary>
         /// <param name="argument">Argument value.</param>
         /// <param name="nameOfArgument">Name of the argument.</param>
+        /// <example>
+        /// <para>How to validate argument `enumerableArgument`.</para>
+        /// <code>
+        /// <![CDATA[
+        ///     enumerableArgument.ThrowIfIsNullOrEmpty(nameof(enumerableArgument));
+        /// ]]>
+        /// </code>
+        /// </example>
         [AssertionMethod]
         public static void ThrowIfIsNullOrEmpty<T>([ValidatedNotNull]this IEnumerable<T> argument, string nameOfArgument)
         {
