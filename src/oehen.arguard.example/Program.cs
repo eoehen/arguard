8ba45a880795022598cea553162872895fef0518
eoehen <49316770+eoehen@users.noreply.github.com>
2021-05-19 19:28:12 +0200

    Add example project for manual tests (#159)

---
+++ src/oehen.arguard.example/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace oehen.arguard.example
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("oehen.arguard example");
+            Console.ReadKey();
+            TestMethod.TestNullValidation(null);
+            Console.ReadKey();
+        }
+    }
+}
