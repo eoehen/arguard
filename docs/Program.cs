d60b9b7cbb07010edda2636f828df4b847d9b8c6
Pascal Berger <pascal.berger@swissgrc.com>
2025-01-06 20:39:40 +0100

    Switch website from Wyam to Statiq
    # Conflicts:
    #	.gitignore

---
+++ docs/Program.cs
@@ -0,0 +1,11 @@
+﻿return await Bootstrapper
+  .Factory
+  .CreateDocs(args)
+  .AddSetting(Docable.SiteKeys.Logo, "/assets/images/Icon.png")
+  .AddSetting(Keys.Host, "eoehen.github.io/arguard")
+  .AddSetting(Keys.LinksUseHttps, true)
+  .AddSetting(Docable.SiteKeys.RightSidebarHeadingLevel, 2)
+  .AddSetting(WebKeys.GenerateSearchIndex, true)
+  .AddSetting(WebKeys.AdditionalSearchResultFields, new List<string> { Keys.Excerpt })
+  .AddSetting(Statiq.Markdown.MarkdownKeys.MarkdownExtensions, "bootstrap")
+  .RunAsync();
